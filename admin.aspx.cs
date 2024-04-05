867a4ff4f8fd02af71792e92a64373b9f924b918
AliFrank608 <alifrank0529@gmail.com>
2024-04-04 20:50:03 -0500

    admin aspx updated

--- admin.aspx.cs
+++ admin.aspx.cs
@@ -1,49 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class admin : System.Web.UI.Page
+public partial class admin : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Page load logic here
     }
+
+    private void ToggleVisibility(bool showGrid1, bool showGrid2, bool showGrid3, string labelText, bool showDetails1, bool showDetails2, bool showDetails3)
+    {
+        GridView1.Visible = showGrid1;
+        GridView2.Visible = showGrid2;
+        GridView3.Visible = showGrid3;
+        Label1.Text = labelText;
+        Label1.Visible = true;
+        DetailsView1.Visible = showDetails1;
+        DetailsView2.Visible = showDetails2;
+        DetailsView3.Visible = showDetails3;
+    }
+
     protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
     {
-        GridView1.Visible = true;
-        GridView2.Visible = false; 
-        GridView3.Visible = false;
-        Label1.Text = "Student Summery";
-        Label1.Visible = true;
-        DetailsView2.Visible = false;
-        DetailsView3.Visible = false;
-        DetailsView1.Visible = true;
+        ToggleVisibility(true, false, false, "Student Summary", true, false, false);
     }
+
     protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
     {
-        GridView2.Visible = true;
-        GridView1.Visible = false;
-        GridView3.Visible = false;
-        Label1.Text = "College Summery";
-        Label1.Visible = true;
-        DetailsView1.Visible = false;
-        DetailsView3.Visible = false;
-        DetailsView2.Visible = true;
+        ToggleVisibility(false, true, false, "College Summary", false, true, false);
     }
+
     protected void ImageButton4_Click(object sender, ImageClickEventArgs e)
     {
-        GridView3.Visible = true;
-        GridView1.Visible = false;
-        GridView2.Visible = false;
-        Label1.Text = "Company Summery";
-        Label1.Visible = true;
-        DetailsView1.Visible = false;
-        DetailsView2.Visible = false;
-        DetailsView3.Visible = true;
+        ToggleVisibility(false, false, true, "Company Summary", false, false, true);
     }
+
     protected void ImageButton7_Click(object sender, ImageClickEventArgs e)
     {
         Session.Clear();
